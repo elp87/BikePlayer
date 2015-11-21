3e306830c196793e5de6baa5793c7763fe34f6f4
elp87 <elp1987@gmail.com>
2015-11-21 11:07:21 +0300

    velo: Add IAudioFile

---
+++ BikePlayer/velo/IAudioFile.cs
@@ -0,0 +1,14 @@
+﻿namespace elp87.VeloAudio
+{
+    public interface IAudioFile
+    {
+        void Play();
+        void Pause();
+        void Stop();
+
+        string Artist { get; }
+        string Album { get; }
+        string Year { get; }
+        string Title { get; }
+    }
+}
