ec175a62bdbc98f225e3a886fc7631d491b89311
elp87 <elp1987@gmail.com>
2015-11-24 23:33:59 +0300

    Start Mp3File

---
+++ BikePlayer/velo/Mp3File.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace elp87.VeloAudio
+{
+    public class Mp3File
+    {
+        [DllImport("winmm.dll")]
+        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+
+        private readonly string _filename;
+
+        public Mp3File(string filename)
+        {
+            if (filename == "") return;
+            _filename = filename;
+        }
+
+        public void Play()
+        {
+            Stop();
+            mciSendString("open \"" + _filename + "\" type mpegvideo alias MediaFile", null, 0, IntPtr.Zero);
+            mciSendString("play MediaFile", null, 0, IntPtr.Zero);
+        }
+
+        public void Stop()
+        {
+            mciSendString("close MediaFile", null, 0, IntPtr.Zero);
+        }
+
+        public void Pause()
+        {
+            mciSendString("pause MediaFile", null, 0, IntPtr.Zero);
+        }
+    }
+}
