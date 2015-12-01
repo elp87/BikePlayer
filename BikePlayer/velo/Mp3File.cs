9979521c19918f441b7d4d3e606a29d76515dc89
elp87 <elp1987@gmail.com>
2015-12-01 18:43:40 +0300

    Mp3File: Use NAudio

--- BikePlayer/velo/Mp3File.cs
+++ BikePlayer/velo/Mp3File.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using System.Text;
-using elp87.TagReader;
+﻿using elp87.TagReader;
+using NAudio.Wave;
 
 namespace elp87.VeloAudio
 {
     public class Mp3File : IAudioFile
     {
-        private Mp3Tag _tag;
+        private readonly Mp3Tag _tag;
         private readonly string _filename;
-        //private readonly string _artist, _album, _title;
-        //private readonly int _year;
-
+        private bool _isPaused;
+        private IWavePlayer _waveOutDevice;
+        private AudioFileReader _audioFileReader;
+        
         public Mp3File(string filename)
         {
+            _isPaused = false;
             if (filename == "") return;
             _filename = filename;
             _tag = new Mp3Tag(_filename);
@@ -23,25 +23,31 @@ namespace elp87.VeloAudio
         #region Public
         public void Play()
         {
-            Stop();
-            mciSendString("open \"" + _filename + "\" type mpegvideo alias MediaFile", null, 0, IntPtr.Zero);
-            mciSendString("play MediaFile", null, 0, IntPtr.Zero);
-        }
-
-        public void Stop()
-        {
-            mciSendString("close MediaFile", null, 0, IntPtr.Zero);
+            _waveOutDevice = new WaveOut();
+            _audioFileReader = new AudioFileReader(_filename);
+            _waveOutDevice.Init(_audioFileReader);
+            _waveOutDevice.Play();
         }
 
         public void Pause()
         {
-            mciSendString("pause MediaFile", null, 0, IntPtr.Zero);
-        }
-        #endregion
+            if (!_isPaused)
+            {
+                _waveOutDevice.Pause();
+                _isPaused = true;
+            }
+            else
+            {
+                _waveOutDevice.Play();
+                _isPaused = false;
+            }
 
-        #region Private
-        [DllImport("winmm.dll")]
-        private static extern long mciSendString(string strCommand, StringBuilder strReturn, int iReturnLength, IntPtr hwndCallback);
+        }
+
+        public void Stop()
+        {
+            
+        }
         #endregion
         #endregion
 
