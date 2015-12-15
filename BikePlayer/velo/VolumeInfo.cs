0486d99b56dd196fa84de706fee8067db2ae8523
elp87 <elp1987@gmail.com>
2015-12-15 13:09:38 +0300

    velo: Add VolumeInfo

---
+++ BikePlayer/velo/VolumeInfo.cs
@@ -0,0 +1,14 @@
+﻿namespace elp87.VeloAudio
+{
+    public class VolumeInfo
+    {
+        public VolumeInfo()
+        {
+            Value = 50;
+        }
+
+        public int MinValue { get { return 0; } }
+        public int MaxValue { get { return 100; } }
+        public int Value { get; set; }
+    }
+}
